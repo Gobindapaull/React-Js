c1b8c426fcce9a662f535352e5ad2f76aeee28df
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-04-06 14:38:15 +0530

    Update button.js

--- src/button.js
+++ src/button.js
@@ -1,1 +1,11 @@
- <button class="mx-10 my-10 text-white bg-black px-5 py-3 rounded-full hover:bg-slate-500">Connect</button>
+ function Button() {
+  return (
+    <div className="App">
+    <button class="mx-10 my-10 text-white bg-black px-5 py-3 rounded-full hover:bg-slate-500">Connect</button>
+    </div>
+  );
+}
+
+export default Button;
+
+
