6e28fbb0073dfd57adf26fa98394665b1986cb27
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-08-08 17:54:28 +0530

    Create Staking.js

---
+++ src/Components/Staking.js
@@ -0,0 +1,28 @@
+
+const Dapp = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Staking <span>Dapp</span></h1>
+
+        <h3>
+          <span className="staking">Staking</span> Contract Address:
+          <br />
+          0x2070B15982468939C0C84D7c24d680A3398cf037
+        </h3>
+        <h4>
+          ====================
+        </h4>
+        <h3>
+          <span id="token">Token</span> Contract Address:
+          <br />
+          0x3E691bfD5242a2381dF65C56edaBd19eD22a9f7A
+        </h3>
+
+        <h4>Made by <span>@autoboyt</span></h4>
+      </header>
+    </div>
+  );
+}
+
+export default Dapp;
