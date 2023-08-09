553b22d79e48b86780bd88150876ca57ddbe32b3
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-08-09 09:31:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     App js
+      <Card
+      imgSrc="https://picsum.photos/300/200"
+      imgAlt="Card Image"
+      title="Random Image one"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta expedita
+      eligendi, laudantium facere iusto libero non cupiditate? Ut repellat
+      blanditiis cupiditate illo voluptate perspiciatis exercitationem iste
+      quos vel? Doloremque maiores accusamus blanditiis ratione veritatis
+      molestiae, consequuntur, illum quos, ipsa corporis non dolorem earum
+      dicta consectetur quo nulla voluptatem tenetur expedita."
+      buttonText="Change Image"
+      link="cardPage"
+  />
     </div>
   );
 }
