ab10fc18df507dd02b9543ad2346d05c0301b049
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-12-02 03:07:15 +0530

    Update App.js

--- RandomNumber/App.js
+++ RandomNumber/App.js
@@ -2,9 +2,11 @@ import './App.css';
 
 function App() {
   const randomNumber = Math.random().toFixed(9);
+  const date = new Date();
   return (
     <div className="App">
-     Random Number : {randomNumber}
+     Random Number : {randomNumber}<br /><br />
+     Date : {new Date(Date.now()).toString()}
     </div>
   );
 }
