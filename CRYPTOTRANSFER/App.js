6a437003d2ea06bcca26d950b802355205fda0aa
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-12-02 05:27:22 +0530

    Create App.js

---
+++ CRYPTOTRANSFER/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Transfer from './components/Transfer';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Transfer />
+    </div>
+  );
+}
+
+export default App;
