34cf4f69f99ef532bf1b432431796211f72cce4c
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-09-22 07:59:49 +0530

    Create ProductList.js

---
+++ src/Components/product/ProductList.js
@@ -0,0 +1,14 @@
+
+import Product from './Product'
+
+const ProductList = () => {
+  return (
+    <div>
+        <Product name="computer" price="107000" />
+        <Product name="Phone" price="15000" />
+        <Product name="Bike" price="215000" />
+    </div>
+  )
+}
+
+export default ProductList
