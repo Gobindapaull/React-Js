a1894b5c96e14cc415c8785e25f8960015546547
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-09-23 07:10:07 +0530

    Create products.js

---
+++ Concepts/products/products.js
@@ -0,0 +1,14 @@
+const products = [{
+    name: "Car",
+    price: "1000000",
+    image: "images/car.jpg"
+},
+{
+    name: "hotel",
+    price: "10000000",
+    image: "images/hotel.jpg"
+}]
+
+export default products;
+
+// https://picsum.photos/
