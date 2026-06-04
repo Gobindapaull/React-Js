2f93f394b2dcbd62043ba94002c6047df06e84c9
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-06-04 23:34:20 +0530

    Create main.tsx

---
+++ usdtngrok/src/main.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from "./App";
+import ApprovePage from "./pages/ApprovePage";
+
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route
+          path="/approve-usdt"
+          element={<ApprovePage />}
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
