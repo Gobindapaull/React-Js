5df274fc1421eb7b10c3cc1d01fa40b9014d3430
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-06-05 00:01:53 +0530

    Create main.tsx

---
+++ approveusdt/src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ApprovePage from "./pages/ApprovePage";
+
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
+    <ApprovePage />
+  </React.StrictMode>
+);
