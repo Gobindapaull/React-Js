74f1786c529ecc2ed1c2b61b1d0b5498e72d1059
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-06-29 22:25:00 +0530

    Create coins.ts

---
+++ readme/CryptoCard/data/coins.ts
@@ -0,0 +1,14 @@
+import type { Coin } from "../interfaces/Coin";
+
+export const coins: Coin[] = [
+    {
+    id: 1,
+    name: "Bitcoin",
+    symbol: "BTC",
+    price: "$108,420",
+    change: "+3.20%",
+    marketCap: "$2.1T",
+    volume: "$38B",
+    image: "/bitcoin.png",
+  }
+]
