caab170629a9033032c2b6855ca9e91fa79ca5a0
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-06-29 22:25:47 +0530

    Create Coin.ts

---
+++ readme/CryptoCard/interfaces/Coin.ts
@@ -0,0 +1,10 @@
+export interface Coin {
+    id: number;
+    name: string;
+    symbol: string;
+    price: string;
+    change: string;
+    marketCap: string;
+    volume: string;
+    image: string;
+}
